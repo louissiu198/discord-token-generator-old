c442af92db9c69283bab99258c62fe262d037c80
louissiu <130300063+louissiu198@users.noreply.github.com>
2023-07-11 15:10:25 +0800

    Update captcha.py

--- captcha.py
+++ captcha.py
@@ -1,160 +1,145 @@
 from hcaptcha.collectHsw import FunctionHsw
 from hcaptcha.motionData import MotionData
 from urllib.parse import urlencode
+from helper import Utils, Solver
 from random import randint, choice
-from httpx import get
-from json import dumps
+from json import dumps, loads
 from time import time
-
+import json
 import os
-from helper import Utils, Completion
-
-INFORMATIONS = {
-    "USERAGENT": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36",
-    "HSWSCRIPT": "https://newassets.hcaptcha.com/c/44fa09c/hsw.js",
-    "HAPSCRIPT": "https://hcaptcha.com/1/api.js"
-}
 
 class Captcha:
     def __init__(self, session, userAgent):
-
         self.session = session
-        self.version = Utils.findtheVersion()
-        self.answers = []
-        self.trained = open("./captcha/output/trainedModel.txt").read().splitlines()
+        self.userAgent = userAgent
+        self.version = Utils.getCaptchaV()
+        self.solvedAnswers = []
+        self.textLibrary = open("./hcaptcha/text.txt").read().splitlines()
+
+        self.payload = {
+            "v": self.version,
+            "sitekey": "4c672d35-0701-42b2-88c3-78380b0db560"
+        }
+
+        self.headers = {
+            "Accept": "application/json",
+            "Accept-Encoding": "gzip, deflate, br",
+            "Accept-Language": "en-GB,en;q=0.9",
+            "Content-Length": "0",
+            "Content-Type": "text/plain",
+            "Dnt": "1",
+            "Origin": "https://newassets.hcaptcha.com",
+            "Referer": "https://newassets.hcaptcha.com",
+            "Sec-Ch-Ua": "\"Not.A\/Brand\";v=\"8\", \"Chromium\";v=\"114\", \"Google Chrome\";v=\"114\"".replace("114", self.userAgent.split('Chrome/')[1].split('.0.0.0')[0]),
+            "Sec-Ch-Ua-Mobile": "?0",
+            "Sec-Ch-Ua-Platform": "Windows",
+            "Sec-Fetch-Dest": "empty",
+            "Sec-Fetch-Mode": "cors",
+            "Sec-Fetch-Site": "same-site",
+            "User-Agent": self.userAgent
+        }
+        self.getSetup()
 
     def getSetup(self):
-        self.userAgent = get("https://www.whatismybrowser.com/guides/the-latest-user-agent/chrome").text
-        self.userAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/" + self.userAgent.split("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/")[1].split(".0.0.0 Safari/537.36")[0] + ".0.0.0 Safari/537.36"
-        self.collEcter = FunctionHsw([1900,2000], self.userAgent)
+        self.collEcter = FunctionHsw([1920,1080], self.userAgent)
         self.motionData = MotionData(self.userAgent)
 
     def getConfig(self):
         resp = self.session.post(
-            url = "https://hcaptcha.com/checksiteconfig?" + urlencode(self.basePayload),
-            headers = self.baseHeaders,
+            url = f"https://hcaptcha.com/checksiteconfig?v={self.version}&host=discord.com&sitekey=4c672d35-0701-42b2-88c3-78380b0db560&sc=1&swa=1&spst=0",
+            headers = self.headers,
         )
         return resp.json()["c"]
 
     def getCaptcha(self, c):
-        del self.basePayload["sc"]
-        del self.basePayload["swa"]
-        del self.basePayload["spst"]
-
-        self.basePayload["n"] = self.collEcter.getToken(c["req"]) 
-        self.basePayload["c"] = json.dumps(c)
-        self.basePayload["hl"] = "en"
-        self.basePayload["motionData"] = self.motionData.O1()
-
-        self.baseHeaders["Content-Type"] = "application/x-www-form-urlencoded"
-        self.baseHeaders["Content-Length"] = str(len(self.basePayload))
-
-        resp = self.session.post(
-            url = "https://hcaptcha.com/getcaptcha/" + "4c672d35-0701-42b2-88c3-78380b0db560",
-            headers = self.baseHeaders,
-            data = self.basePayload
-        ).json()
-        try:
-            resp["requester_restricted_answer_set"]
-        except:
-            indexTopic = resp['requester_question']['en']
-            if os.path.isdir("./hcaptcha/images/" + indexTopic) == True:
-                validPath = True
-            else:
-                os.mkdir('./hcaptcha/images/'+ indexTopic)
-            for i in range(len(resp['requester_question_example'])):
-                imageReq =  requests.get(
-                    url = str(resp['requester_question_example'][i]),
-                    stream = True
-                )
-                with open("./hcaptcha/images/"+ indexTopic + "/" + str(random.randint(10000000,90000000)) + ".png", 'wb') as f:
-                    f.write(imageReq.content)
-
-        self.basePayload["n"] = self.getHsw(resp["c"]["req"])
-        self.basePayload["c"] = json.dumps(resp["c"])
-        self.basePayload["action"] = "challenge-refresh"
-        self.basePayload["old_ekey"] = resp["key"]
-        self.basePayload["a11y_tfe"] = "true"
-        self.basePayload["extraData"] = resp
-        self.basePayload["motionData"] = self.motionData.O2(resp["key"])
+        payload = self.payload | {
+            "host": "discord.com",
+            "hl": "en",
+            "a11y_tfe": "true",
+            "motionData": dumps(self.motionData.O1()),
+            "n": self.collEcter.getToken(c["req"]),
+            "c": dumps(c)
+        }
+        
+        self.headers["Content-Type"] = "application/x-www-form-urlencoded"
+        self.headers["Content-Length"] = str(len(urlencode(payload)))
 
         resp = self.session.post(
-            url = "https://hcaptcha.com/getcaptcha/" + "4c672d35-0701-42b2-88c3-78380b0db560",
-            headers = self.baseHeaders,
+            url = "https://hcaptcha.com/getcaptcha/4c672d35-0701-42b2-88c3-78380b0db560",
+            headers = self.headers,
             data = self.basePayload
         )
-        return resp.json()
+        if resp.status_code == 200:
+            return resp.json(), resp.cookies["hmt_id"]
+        else:
+            return None, None
 
     def solveCaptcha(self, question):
+        question = question.lower()
         for i in range(2):
-            if f"{question.lower()}:yes" in self.trained:
+            if f"{question}:yes" in self.textLibrary:
                 return {"text": "yes"}
-            elif f"{question.lower()}:no" in self.trained:
+            elif f"{question}:no" in self.textLibrary:
                 return {"text": "no"}
             else:
                 answer = ""
                 for chunk in Completion.create(question + " strictly respond yes or no", None):
-                    answer = (answer + chunk)
-                answer = answer.lower()
+                    answer = (answer + chunk).lower()
                 if answer.startswith("yes"):
-                    with open("./captcha/output/trainedModel.txt", "a") as f:
-                        self.answers.append(question.lower() + ":" + "yes")
+                    with open("./hcaptcha/text.txt", "a") as f:
+                        self.solvedAnswers.append(question + ":" + "yes")
                     return {"text": "yes"}
                 elif answer.startswith("no"):
-                    self.answers.append(question.lower() + ":" + "yes")
+                    self.solvedAnswers.append(question + ":" + "no")
                     return {"text": "no"}
                 else:
                     continue
                 
-            
-    def checkCaptcha(self, extraData):
-        del self.basePayload["host"]
-        del self.basePayload["action"]
-        del self.basePayload["old_ekey"]
-        del self.basePayload["a11y_tfe"]
-        del self.basePayload["extraData"]
+    def checkCaptcha(self):
+        base = self.getCaptcha(self.getConfig())
+        if base[0] != None:
+            answerList = {}
+            for tasks in base[0]["tasklist"]:
+                answerList[tasks["task_key"]] = self.solveCaptcha(tasks["datapoint_text"]["en"])
 
-        self.basePayload["motionData"] = self.baseMotion2
+            payload = self.payload | {
+                "answers": answerList,
+                "c": dumps(dumps(base[0]["c"])), # escape json
+                "n": self.collEcter.getToken(base[0]["c"]["req"]), 
+                "job_mode": "text_free_entry",
+                "motionData": dumps(dumps(self.motionData.O3())), # escape json
+                "serverdomain": "discord.com"
+            }
 
-        self.baseHeaders["Accept"] = "*/*"
-        self.baseHeaders["Content-Type"] = "application/json;charset=UTF-8"
-        self.baseHeaders["Content-Length"] = str(len(self.basePayload))
+            self.headers["Accept"] = "*/*"
+            self.headers["Cookie"] = f"hmt_id={base[1]};"
+            self.headers["Content-Type"] = "application/json;charset=UTF-8"
+            self.headers["Content-Length"] = str(len(payload))
 
-        c = extraData["c"]
-        old_ekey = extraData["key"]
-        taskList = extraData["tasklist"]
-        answerList = {}
-        for tasks in taskList:
-            answerList[tasks["task_key"]] = self.solveCaptcha(tasks["datapoint_text"]["en"])
-        self.basePayload["answers"] = answerList
-        self.basePayload["job_mode"] = "text_free_entry"
-        self.basePayload["serverdomain"] = self.siteHost
-
-        print(self.basePayload)
-        resp = self.session.post(
-            url = f"https://hcaptcha.com/checkcaptcha/4c672d35-0701-42b2-88c3-78380b0db560/{old_ekey}",
-            headers = self.baseHeaders,
-            json = self.basePayload
-        )
-        if 'pass' in str(resp.json()):
-            if resp.json()["pass"] == True:
-                for i in range(len(self.answers)):
-                    with open("./captcha/output/trainedModel.txt", "a") as f:
-                        f.write(self.answers[i] + "\n")
-                return resp.json()['generated_pass_UUID']
-            elif resp.json()["pass"] == False:
+            resp = self.session.post(
+                url = f"https://hcaptcha.com/checkcaptcha/4c672d35-0701-42b2-88c3-78380b0db560/{base[0]['key']}",
+                headers = self.headers,
+                json = payload
+            )
+            if "pass" in resp.json():
+                rest = loads(resp.text)
+                if rest["pass"] == True:
+                    return resp.json()["generated_pass_UUID"]
+                else:
+                    return False
+            else:
                 return False
-        else:
-            return False
-    
-    def createTask(self):
-        c = self.getConfig()
-        g = self.getCaptcha(c)
-        f = self.checkCaptcha(g)
-        if f != False:
-            return f
-        else:
-            return False
+
+if __name__  == "__main__":
+    from tls_client import Session
+    client = Session(
+        client_identifier="chrome114",
+        random_tls_extension_order=True
+    )
+    sexxion = Captcha(client, "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36")
+    print(sexxion.getCaptcha())
+
+
         # except Exception as e:
         #     # print(e)
         #     return False
