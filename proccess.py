8eeee2a9f7270326175d8c8f257c2164f76c2d90
louissiu <130300063+louissiu198@users.noreply.github.com>
2023-07-18 14:31:21 +0200

    Update proccess.py
    Signed-off-by: louissiu <130300063+louissiu198@users.noreply.github.com>

--- proccess.py
+++ proccess.py
@@ -54,21 +54,7 @@ class Proccess:
     
     def getRegister(self):
         while True:
-            self.headers = {   
-                "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
-                "Accept-Encoding": "gzip, deflate, br",
-                "Accept-Language": "en-GB,en;q=0.9",
-                "Dnt": "1",
-                "Sec-Ch-Ua": "\"Not.A\/Brand\";v=\"8\", \"Chromium\";v=\"114\", \"Google Chrome\";v=\"114\"",
-                "Sec-Ch-Ua-Mobile": "?0",
-                "Sec-Ch-Ua-Platform": "Windows",
-                "Sec-Fetch-Dest": "document",
-                "Sec-Fetch-Mode": "navigate",
-                "Sec-Fetch-Site": "none",
-                "Sec-Fetch-User": "?1",
-                "Upgrade-Insecure-Requests": "1",
-                "User-Agent": self.userAgent
-            }
+            self.headers = Utils.getHeaders(self.userAgent)
             self.captchaKey = None
             if self.captchaKey == None:
                 break
