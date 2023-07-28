3bf8dcec62ea2b12a48d6552dc669a6db63e4f8e
louissiu <130300063+louissiu198@users.noreply.github.com>
2023-07-28 14:05:54 +0200

    Update collectHsw.py
    Signed-off-by: louissiu <130300063+louissiu198@users.noreply.github.com>

--- hcaptcha/collectHsw.py
+++ hcaptcha/collectHsw.py
@@ -6,9 +6,10 @@ class FunctionHsw:
     def __init__(self, whList, userAgent, opTion, inVite = None):
         self.options = Options()
         self.options.add_argument("--headless") 
-        self.options.add_argument("--incognito")
+        # self.options.add_argument("--incognito")
         self.options.add_argument("--no-sandbox")
         self.options.add_argument("--single-process")
+        self.options.add_argument(f"user-agent={userAgent}")
         self.options.add_argument("--disable-dev-shm-usage")
         self.options.add_argument("disable-infobars")
         self.options.add_argument("--disable-blink-features=AutomationControlled")
@@ -17,14 +18,17 @@ class FunctionHsw:
         self.script = get("https://newassets.hcaptcha.com/c/c2b994f/hsw.js").text
         self.driver = webdriver.Chrome(options=self.options)
         self.prePared(opTion, inVite)
+        self.driver.set_window_size(whList[0],whList[1])
+
 
     def prePared(self, choice, invite):
-        if choice == 1:
-            self.driver.get("https://discord.com/register")
-        elif choice == 2:
-            self.driver.get("https://discord.com/register")
-        elif choice == 3:
-            self.driver.get("https://discord.com/invite/" + invite)
+        self.driver.get("https://hcaptcha.projecttac.com/?sitekey=4c672d35-0701-42b2-88c3-78380b0db560")
+        # if choice == 1:
+        #     self.driver.get("https://discord.com/register")
+        # elif choice == 2:
+        #     self.driver.get("https://discord.com/register")
+        # elif choice == 3:
+        #     self.driver.get("https://discord.com/invite/" + invite)
     
     def getToken(self, req):
         return self.driver.execute_script(f'{self.script}; return hsw("{req}")')
@@ -34,5 +38,5 @@ class FunctionHsw:
         self.driver.quit()
 
 if __name__ == "__main__":
-    client = FunctionHsw([1920,1080], "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36")
+    client = FunctionHsw([1920,1080], "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36", 1, None)
     input()
